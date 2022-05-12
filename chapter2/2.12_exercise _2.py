bdfc6dd162a79f2abf34ae71bf534246e0400c7e
Liliia <myhaliuklilia@gmail.com>
2022-05-12 20:55:26 +0300

    Rename and sort files into folders for better understanding

---
+++ chapter2/2.12_exercise _2.py
@@ -0,0 +1,26 @@
+#Exercises of the book "Think python"
+#2.12 Exercises
+
+#Exercise 2
+# "Assume that we execute the following assignment statements: 
+
+width = 17
+height = 12.0
+delimiter = '.'
+
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+
+# 1. width/2
+print('My guess is 8.5. And the real answer is: ', (width/2))
+
+# 2. width/2.0
+print('My guess is 8.5. And the real answer is: ', (width/2.0))
+
+# height/3
+print('My guess is 4.0. And the real answer is: ', (height/3))
+
+# 1 + 2 * 5
+print('My guess is 11. And the real answer is: ', (1 + 2 * 5))
+
+# delimiter * 5
+print("My guess is '.....'. And the real answer is: ", (delimiter * 5))
