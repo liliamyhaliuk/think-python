bfea7f92899f786020de11ac1cc439bf1e326b17
Liliia <myhaliuklilia@gmail.com>
2022-07-18 21:50:43 +0300

    13.1.4, 13.1.5

--- chapter13/13.1.4.py
+++ chapter13/13.1.4.py
@@ -1,10 +1,11 @@
 '''
 Exercises of the book "Think python"
-13.1.3 Exercise:
+13.1.4 Exercise:
 '''
 
-# Modify the program from the previous exercise to print the 20 most frequently used words in
-# the book.
+# Modify the previous program to read a word list (see Section 9.1) and then print all the words
+# in the book that are not in the word list. How many of them are typos? How many of them are
+# common words that should be in the word list, and how many of them are really obscure?
 
 import os
 import string
@@ -19,9 +20,6 @@ for c in punct:
 
 # Get a path to the file with text
 PATH = os.path.sep.join(["chapter13", "gunetberg.txt"])
-
-# Easy test case (Uncomment)
-#PATH = os.path.sep.join(["chapter13", "test.txt"])
 
 # Delete punctuation. Lower case. Split words into list
 all_words = {}
@@ -46,16 +44,24 @@ with open(PATH, 'r') as f:
             if all_words.get(word) is not None:
                 all_words[word] = all_words[word] + 1
 
-# Set counter for 20 words
+# Get the pass to the word list
+PATH2 = os.path.sep.join(["chapter13", "words.txt"])
+
+# Create dict of real words from word_list
+word_dict = {}
+with open(PATH2, 'r') as f2:
+    for i in f2:
+        word_dict[i.strip().lower()] = ""
+
+# Find all words from the book that are not in the word list
+not_words = {}
+for word, frequency in all_words.items():
+    if word_dict.get(word) is None:
+        not_words[word] = frequency
+
+# Print words with frequency more than 1. These are possible real words.
 counter = 0
-
-# Go through sorted by value dictionary
-for k in sorted(all_words, key=all_words.get, reverse=True):
-    print(f"{k} : {all_words[k]}")
-
-    # Print only 20 most frequent words
-    counter += 1
-    if counter > 20:
+for k in sorted(not_words, key=all_words.get, reverse=True):
+    if not_words[k] < 2:
         break
-
-print("Total amount of words: ", len(all_words))
+    print(f"{k} : {not_words[k]}")
